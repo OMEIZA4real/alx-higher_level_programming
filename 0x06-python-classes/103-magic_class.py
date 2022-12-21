d6a88a727047b66842c0a5dddc58fd7cb36f46ac
OMEIZA4real <omzy4oakland@gmail.com>
2022-12-21 10:35:08 -0800

    100-singly_linked_list.py, 101-square.py, 102-square.py, 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+""" declares a class called MagicClass """
 import math
 
 
 class MagicClass:
-    """ Magic Class for a circle object"""
-
+    """ declaration of __init__ function """
     def __init__(self, radius=0):
-        """ Constructor of Class """
-        self.__radius = 0
+        """ initialization of _MagicClass__radius with 0 """
+        self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
-        self.__radius = radius
-
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
+    """ defines a function named area """
     def area(self):
-        """ Area calculation """
-        return (self.__radius ** 2 * math.pi)
-
+        """ returns area """
+        return self._MagicClass__radius ** 2 * math.pi
+    """ defines a function named circumference """
     def circumference(self):
-        """ Circumference Calculation """
-        return (2 * math.pi * self.__radius)
+        """ returns circumference """
+        return 2 * math.pi * self._MagicClass__radius
