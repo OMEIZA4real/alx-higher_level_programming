25811dba4729cbdc5f8b54d7c712426b059f24df
OMEIZA4real <omzy4oakland@gmail.com>
2023-03-31 22:00:56 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in
-the header of the response.
-"""
-
+""" POST an email with requests """
 
 if __name__ == "__main__":
-    from requests import get
-    from sys import argv
-
-    html = get(argv[1])
-    print(html.headers.get('X-Request-Id'))
+    import requests
+    import sys
+    url = sys.argv[1]
+    data = {"email": sys.argv[2]}
+    r = requests.post(url, data)
+    body = r.text
+    print(body)
